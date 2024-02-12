c898b9e27eae346d26e0b879fe31eef26478d795
skulkarni207 <skulkarni207@IN-4H3K9S3>
2024-02-12 12:00:51 +0530

    Add project files.

---
+++ ConsoleApp3/Employee.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp3
+{
+	public class Employee
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public decimal Salary { get; set; }
+
+
+		
+		public static string Serialize(Employee employee)
+		{
+			return JsonConvert.SerializeObject(employee);
+		}
+
+		public static Employee Deserialize(string jsonData)
+		{
+			return JsonConvert.DeserializeObject<Employee>(jsonData);
+		}
+	}
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		
+				// Serializaion
+				Employee empObj = new Employee();
+				empObj.ID = 1;
+				empObj.Name = "Soujanya";
+				empObj.Salary = 30000;
+
+
+			// Convert Employee object to JOSN string format
+			string serializedData = Employee.Serialize(empObj);
+			Console.WriteLine(serializedData);
+
+
+			//deserialization
+			Employee deserializedData = Employee.Deserialize(serializedData);
+			Console.WriteLine(deserializedData.ID);
+			Console.WriteLine(deserializedData.Name);
+			Console.WriteLine(deserializedData.Salary);
+			Console.ReadLine();
+
+
+
+		}
+	}
+}
